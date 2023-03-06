c05292819c3f518d4c15566392ba543899ba3ff6
Daniel Zúñiga <1danielarmando.1999@gmail.com>
2023-03-05 22:10:40 -0500

    Simulaciones de las 3 visiones de daltonismo, terminadas.

--- sketch/sketch4.js
+++ sketch/sketch4.js
@@ -1,11 +1,14 @@
 let originalImage;
 let modifiedImage;
+let proposedImage;
 
 function preload() {
   originalImage = loadImage("../assets/sketch/2/prueba.png", function () {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
+    proposedImage = originalImage.get();
+    proposedImage.loadPixels();
   });
 }
 
@@ -18,19 +21,14 @@ function setup() {
   let button1 = createButton("Deuteranopia");
   button1.position(80, 40);
   button1.mousePressed(function () {
+    originalImage.loadPixels();
+    modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
     for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
       let g = modifiedImage.pixels[i * 4 + 1];
       let r = modifiedImage.pixels[i * 4 + 0];
-      if(g>r){
-        modifiedImage.pixels[i * 4 + 0] = g;
-        modifiedImage.pixels[i * 4 + 1] = g;
-      }else if(r>g){
-        modifiedImage.pixels[i * 4 + 0] = r;
-        modifiedImage.pixels[i * 4 + 1] = r;
-      }else{
-        modifiedImage.pixels[i * 4 + 2] -= 200;
-      }
+      modifiedImage.pixels[i * 4 + 1] = (g + r) / 2;
+      modifiedImage.pixels[i * 4 + 0] = (g + r) / 2;
     }
     modifiedImage.updatePixels();
   });
@@ -39,14 +37,78 @@ function setup() {
   let button2 = createButton("Protanopia");
   button2.position(button1.x + button1.width + 10, 40);
   button2.mousePressed(function () {
-    //Función del botón 2
+    originalImage.loadPixels();
+    modifiedImage = originalImage.get();
+    modifiedImage.loadPixels();
+    for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
+      let g = modifiedImage.pixels[i * 4 + 1];
+      let r = modifiedImage.pixels[i * 4 + 0];
+      let b = modifiedImage.pixels[i * 4 + 2];
+
+      if (g > r && g > b) {
+        modifiedImage.pixels[i * 4 + 1] = (r + g) / 2 + 120;
+        modifiedImage.pixels[i * 4 + 0] = (r + g) / 2 + 120;
+        modifiedImage.pixels[i * 4 + 2] = 0;
+      } else if (g > r && b > g) {
+        modifiedImage.pixels[i * 4 + 1] = g + g * 0.5;
+        modifiedImage.pixels[i * 4 + 0] = 0;
+        modifiedImage.pixels[i * 4 + 2] = b + b * 0.5;
+      } else if (r > g && g > b && r > b) {
+        if (abs(r - g) < 20) {
+          modifiedImage.pixels[i * 4 + 1] = (r + g) / 2;
+          modifiedImage.pixels[i * 4 + 0] = (r + g) / 2;
+        } else {
+          modifiedImage.pixels[i * 4 + 1] = (r + g) / 2 - 20;
+          modifiedImage.pixels[i * 4 + 0] = (r + g) / 2 - 20;
+        }
+      } else if (r > g && b > g) {
+        modifiedImage.pixels[i * 4 + 1] = g + 2 + (g + 2) * 0.9;
+        modifiedImage.pixels[i * 4 + 0] = 0;
+        modifiedImage.pixels[i * 4 + 2] = b + b * 0.9;
+      } else if (r > g && r > b) {
+        modifiedImage.pixels[i * 4 + 1] = r - 150;
+        modifiedImage.pixels[i * 4 + 0] = r - 150;
+      }
+    }
+    modifiedImage.updatePixels();
   });
 
   //Botón3
   let button3 = createButton("Tritanopia");
   button3.position(button2.x + button2.width + 10, 40);
   button3.mousePressed(function () {
-    //Función del botón 2
+    originalImage.loadPixels();
+    modifiedImage = originalImage.get();
+    modifiedImage.loadPixels();
+
+    for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
+      let g = modifiedImage.pixels[i * 4 + 1];
+      let r = modifiedImage.pixels[i * 4 + 0];
+      let b = modifiedImage.pixels[i * 4 + 2];
+
+      if (b > r && b > g) {
+        if (r >= g) {
+          modifiedImage.pixels[i * 4 + 2] = 0;
+        } else {
+          modifiedImage.pixels[i * 4 + 2] = (g + b) / 2 - 50;
+          modifiedImage.pixels[i * 4 + 1] = (g + b) / 2 - 50;
+        }
+      } else if (b > r && g > b) {
+        modifiedImage.pixels[i * 4 + 1] = (g + b) / 2 + 50;
+        modifiedImage.pixels[i * 4 + 2] = (g + b) / 2 + 50;
+      } else if (r > b && g > b) {
+        if (abs(r - g) < 20) {
+          modifiedImage.pixels[i * 4 + 0] = r;
+          modifiedImage.pixels[i * 4 + 1] = g - g * 0.1;
+          modifiedImage.pixels[i * 4 + 2] = b + 250;
+        } else {
+          modifiedImage.pixels[i * 4 + 0] = r;
+          modifiedImage.pixels[i * 4 + 1] = g - g * 0.1;
+          modifiedImage.pixels[i * 4 + 2] = b + 130;
+        }
+      }
+    }
+    modifiedImage.updatePixels();
   });
 
   //Botón4
@@ -56,6 +118,8 @@ function setup() {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
+    proposedImage = originalImage.get();
+    proposedImage.loadPixels();
   });
 }
 
@@ -79,13 +143,19 @@ function draw() {
 
   image(originalImage, 0, 200, oImgWidth, oImgHeight);
 
-  textSize(30);
+  textSize(25);
   fill(0);
   text("Imagen Original", oImgWidth / 4, 170);
 
-  image(modifiedImage, oImgWidth + 100, 200, oImgWidth, oImgHeight);
+  image(modifiedImage, oImgWidth + 50, 200, oImgWidth, oImgHeight);
 
-  textSize(30);
+  textSize(25);
   fill(0);
-  text("Imagen Modificada", oImgWidth + 100 + oImgWidth / 4, 170);
+  text("Como vería", oImgWidth + 100 + oImgWidth / 4, 170);
+
+  image(proposedImage, 2 * (oImgWidth + 50), 200, oImgWidth, oImgHeight);
+
+  textSize(25);
+  fill(0);
+  text("Imagen modificada", 2 * (oImgWidth + 50) + oImgWidth / 4, 170);
 }
