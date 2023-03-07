43e87703744937052240d156418547e40414f4cd
Daniel Zúñiga <1danielarmando.1999@gmail.com>
2023-03-07 08:43:44 -0500

    Deuteranopia lista

--- sketch/sketch4.js
+++ sketch/sketch4.js
@@ -27,8 +27,21 @@ function setup() {
     for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
       let g = modifiedImage.pixels[i * 4 + 1];
       let r = modifiedImage.pixels[i * 4 + 0];
-      modifiedImage.pixels[i * 4 + 1] = (g + r) / 2;
-      modifiedImage.pixels[i * 4 + 0] = (g + r) / 2;
+      //let b = modifiedImage.pixels[i * 4 + 2];
+      modifiedImage.pixels[i * 4 + 1] = ((g + r) / 2);
+      modifiedImage.pixels[i * 4 + 0] = ((g + r) / 2);
+      g = modifiedImage.pixels[i * 4 + 1];
+      if(g<=130){
+        if(g + g*0.4 >= 130){
+          modifiedImage.pixels[i * 4 + 1] = 130;
+          modifiedImage.pixels[i * 4 + 0] = 130;
+        }
+        modifiedImage.pixels[i * 4 + 1] = g + g*0.4;
+        modifiedImage.pixels[i * 4 + 0] = g + g*0.4;
+      }
+      modifiedImage.pixels[i * 4 + 1] *= 0.9;
+      modifiedImage.pixels[i * 4 + 0] *= 0.9;
+      modifiedImage.pixels[i * 4 + 2] *= 0.9;
     }
     modifiedImage.updatePixels();
   });
@@ -40,60 +53,14 @@ function setup() {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
-    /*
-    for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
-      let r = modifiedImage.pixels[i * 4 + 0];
-      let g = modifiedImage.pixels[i * 4 + 1];
-      let b = modifiedImage.pixels[i * 4 + 2];
-      if(r>=g){
-        modifiedImage.pixels[i * 4 + 0] = g;
-      }
-
-      //modifiedImage.pixels[i * 4 + 0] = (g + r) / 2;
-      //modifiedImage.pixels[i * 4 + 2] = b + (b*0.9);
-    }
-*/
-
-    /*
-    for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
-      let g = modifiedImage.pixels[i * 4 + 1];
-      let r = modifiedImage.pixels[i * 4 + 0];
-      let b = modifiedImage.pixels[i * 4 + 2];
-
-      if (g > r && g > b) {
-        modifiedImage.pixels[i * 4 + 1] = (r + g) / 2 + 120;
-        modifiedImage.pixels[i * 4 + 0] = (r + g) / 2 + 120;
-        modifiedImage.pixels[i * 4 + 2] = 0;
-      } else if (g > r && b > g) {
-        modifiedImage.pixels[i * 4 + 1] = g + g * 0.5;
-        modifiedImage.pixels[i * 4 + 0] = 0;
-        modifiedImage.pixels[i * 4 + 2] = b + b * 0.5;
-      } else if (r > g && g > b && r > b) {
-        if (abs(r - g) < 20) {
-          modifiedImage.pixels[i * 4 + 1] = (r + g) / 2;
-          modifiedImage.pixels[i * 4 + 0] = (r + g) / 2;
-        } else {
-          modifiedImage.pixels[i * 4 + 1] = (r + g) / 2 - 20;
-          modifiedImage.pixels[i * 4 + 0] = (r + g) / 2 - 20;
-        }
-      } else if (r > g && b > g) {
-        modifiedImage.pixels[i * 4 + 1] = g + 2 + (g + 2) * 0.9;
-        modifiedImage.pixels[i * 4 + 0] = 0;
-        modifiedImage.pixels[i * 4 + 2] = b + b * 0.9;
-      } else if (r > g && r > b) {
-        modifiedImage.pixels[i * 4 + 1] = r - 150;
-        modifiedImage.pixels[i * 4 + 0] = r - 150;
-      }
-    }
-*/
-
     for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
       let r = modifiedImage.pixels[i * 4 + 0];
       let g = modifiedImage.pixels[i * 4 + 1];
       let b = modifiedImage.pixels[i * 4 + 2];
 
-      modifiedImage.pixels[i * 4 + 1] = (g + b) / 2 ;
-      modifiedImage.pixels[i * 4 + 2] = (g + b) / 2 ;
+      modifiedImage.pixels[i * 4 + 1] = (g + r) / 2 ;
+      modifiedImage.pixels[i * 4 + 0] = (g + r) / 2 ;
+      
     }
 
     modifiedImage.updatePixels();
