0537eed53eb7d1c26e21f4bfe2965ed07a30c60e
Daniel Zúñiga <1danielarmando.1999@gmail.com>
2023-03-12 22:12:40 -0500

    Mapeo en proceso

--- sketch/sketch4.js
+++ sketch/sketch4.js
@@ -5,19 +5,45 @@ let matrixRGB_LMS;
 let matrixRGB_LMSInverse;
 let matrixProt;
 let matrixDeut;
+let matrixProtXDeut;
+let ap;
+let ad;
 
 function preload() {
-  originalImage = loadImage("../assets/sketch/2/prueba.png", function () {
+  originalImage = loadImage("../assets/sketch/2/prueba.jpg", function () {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
     proposedImage = originalImage.get();
     proposedImage.loadPixels();
   });
-  matrixRGB_LMS = [[17.8824, 43.5161, 4.1194], [3.4557, 27.1554, 3.8671], [0.0300, 0.1843, 1.4671]];
-  matrixRGB_LMSInverse = [[0.0809, -0.1305, 0.1167], [-0.0102, 0.0540, -0.1136], [-0.0004, -0.0041, 0.6935]];
-  matrixProt = [[0, 2.0234, -2.5258],[0, 1, 0],[0, 0, 1]];
-  matrixDeut = [[1, 0, 0],[0.4942, 0, 1.2483],[0, 0, 1]];
+  ap = 1;
+  ad = 1;
+  matrixRGB_LMS = [
+    [17.8824, 43.5161, 4.1194],
+    [3.4557, 27.1554, 3.8671],
+    [0.03, 0.1843, 1.4671],
+  ];
+  matrixRGB_LMSInverse = [
+    [0.0809, -0.1305, 0.1167],
+    [-0.0102, 0.054, -0.1136],
+    [-0.0004, -0.0041, 0.6935],
+  ];
+  matrixProt = [
+    [1 - ap, 2.0234 * ap, -2.5258 * ap],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
+  matrixDeut = [
+    [1, 0, 0],
+    [0.4942 * ad, 1 - ad, 1.2483 * ad],
+    [0, 0, 1],
+  ];
+  matrixProtXDeut = [
+    [1 - ap, 2.0234 * ap, -2.5258 * ap],
+    [0.4942 * ad, 1 - ad, 1.2483 * ad],
+    [0, 0, 1],
+  ];
 }
 
 function multiplyMatrices(a, b) {
@@ -43,7 +69,6 @@ function multiplyMatrices(a, b) {
   return result;
 }
 
-
 function setup() {
   createCanvas(1341, 680);
 
@@ -61,7 +86,7 @@ function setup() {
       let r = modifiedImage.pixels[i * 4 + 0];
       let g = modifiedImage.pixels[i * 4 + 1];
       let b = modifiedImage.pixels[i * 4 + 2];
-      let matrixRGB = [[r],[g],[b]];
+      let matrixRGB = [[r], [g], [b]];
       let matrixLMS = multiplyMatrices(matrixRGB_LMS, matrixRGB);
       let matrixLMS_modified = multiplyMatrices(matrixDeut, matrixLMS);
       let result = multiplyMatrices(matrixRGB_LMSInverse, matrixLMS_modified);
@@ -85,7 +110,7 @@ function setup() {
       let r = modifiedImage.pixels[i * 4 + 0];
       let g = modifiedImage.pixels[i * 4 + 1];
       let b = modifiedImage.pixels[i * 4 + 2];
-      let matrixRGB = [[r],[g],[b]];
+      let matrixRGB = [[r], [g], [b]];
       let matrixLMS = multiplyMatrices(matrixRGB_LMS, matrixRGB);
       let matrixLMS_modified = multiplyMatrices(matrixProt, matrixLMS);
       let result = multiplyMatrices(matrixRGB_LMSInverse, matrixLMS_modified);
@@ -95,12 +120,47 @@ function setup() {
     }
 
     modifiedImage.updatePixels();
+
+    proposedImage = originalImage.get();
+    proposedImage.loadPixels();
+
+    for(let i = 0; i < proposedImage.width * proposedImage.height; i++){
+      let r = proposedImage.pixels[i * 4 + 0];
+      let g = (proposedImage.pixels[i * 4 + 1] + r)/2;
+      let b = (proposedImage.pixels[i * 4 + 2] + r)/2;
+
+
+    }
+
+    proposedImage.updatePixels();
   });
 
   //Botón3
-  let button3 = createButton("Tritanopia");
+  let button3 = createButton("Híbrido: Deut X Prot");
   button3.position(button2.x + button2.width + 10, 40);
   button3.mousePressed(function () {
+    originalImage.loadPixels();
+    modifiedImage = originalImage.get();
+    modifiedImage.loadPixels();
+    for (let i = 0; i < modifiedImage.width * modifiedImage.height; i++) {
+      let r = modifiedImage.pixels[i * 4 + 0];
+      let g = modifiedImage.pixels[i * 4 + 1];
+      let b = modifiedImage.pixels[i * 4 + 2];
+      let matrixRGB = [[r], [g], [b]];
+      let matrixLMS = multiplyMatrices(matrixRGB_LMS, matrixRGB);
+      let matrixLMS_modified = multiplyMatrices(matrixProtXDeut, matrixLMS);
+      let result = multiplyMatrices(matrixRGB_LMSInverse, matrixLMS_modified);
+      modifiedImage.pixels[i * 4 + 0] = result[0][0];
+      modifiedImage.pixels[i * 4 + 1] = result[1][0];
+      modifiedImage.pixels[i * 4 + 2] = result[2][0];
+    }
+    modifiedImage.updatePixels();
+  });
+
+  //Botón4
+  let button4 = createButton("Tritanopia");
+  button4.position(button3.x + button3.width + 10, 40);
+  button4.mousePressed(function () {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
@@ -112,16 +172,23 @@ function setup() {
     modifiedImage.updatePixels();
   });
 
-  //Botón4
-  let button4 = createButton("Retornar a original");
-  button4.position(button3.x + button3.width + 10, 40);
-  button4.mousePressed(function () {
+  //Botón5
+  let button5 = createButton("Retornar a original");
+  button5.position(button4.x + button4.width + 10, 40);
+  button5.mousePressed(function () {
     originalImage.loadPixels();
     modifiedImage = originalImage.get();
     modifiedImage.loadPixels();
     proposedImage = originalImage.get();
     proposedImage.loadPixels();
   });
+/*
+  //Slider 1
+  let sliderap = createSlider(0, 1, 0, 0.01);
+  sliderap.input(function() {
+    ap = sliderap.value();
+  });
+*/
 }
 
 function draw() {
@@ -159,4 +226,6 @@ function draw() {
   textSize(25);
   fill(0);
   text("Imagen modificada", 2 * (oImgWidth + 50) + oImgWidth / 4, 170);
+
+  //text("Variable: " + ap, 10, 20);
 }
